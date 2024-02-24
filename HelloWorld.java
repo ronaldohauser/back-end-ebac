2e2775adfc894ab42c117dbb85df5bdbc22a162b
Ronaldo Hauser Dias <57193790+ronaldohauser@users.noreply.github.com>
2024-02-24 18:08:55 -0300

    Primeiro projeto em java do curso de BackEnd

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Classe
+public class HelloWorld {
+
+    // método
+    public static void main(String[] args) {
+        // Impressão no console
+        System.out.println("Hello, World!");
+    }
+}
